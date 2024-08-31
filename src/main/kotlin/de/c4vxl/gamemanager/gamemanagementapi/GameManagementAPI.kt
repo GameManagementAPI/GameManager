d7ae026edea3c6153466615c3549eeb33cb4d387
c4vxl <manage@c4vxl.de>
2024-08-31 11:43:47 +0200

    update GameManagementAPI

--- src/main/kotlin/de/c4vxl/gamemanager/gamemanagementapi/GameManagementAPI.kt
+++ src/main/kotlin/de/c4vxl/gamemanager/gamemanagementapi/GameManagementAPI.kt
@@ -4,39 +4,18 @@ import de.c4vxl.gamemanager.gamemanagementapi.game.Game
 import de.c4vxl.gamemanager.gamemanagementapi.game.GameID
 
 object GameManagementAPI {
-    private val games: MutableMap<String, MutableList<Game>> = mutableMapOf()
-    private val gameIDs: MutableSet<GameID> = mutableSetOf()
+    val games: MutableList<Game> = mutableListOf()
+    private val gameIDs: MutableList<GameID> get() = games.map { it.id }.toMutableList()
 
-    fun getGames(teamAmount: Int, teamSize: Int): List<Game> {
-        val key = "${teamAmount}x$teamSize"
-        return games[key]?.filter { !it.isOver } ?: emptyList()
-    }
+    fun getGames(teamAmount: Int, teamSize: Int): List<Game> = games.filter { it.teamAmount == teamAmount && it.teamSize == teamSize }
 
-    fun getGame(id: GameID): Game? {
-        return games.values.flatten().find { it.id == id }
-    }
+    fun getGame(id: GameID): Game? = games.find { it.id.asString == id.asString }
 
     fun getGame(teamAmount: Int, teamSize: Int): Game {
         return getGames(teamAmount, teamSize).filter { it.isQueuing }.randomOrNull()
             ?: registerGame(Game(teamAmount, teamSize))
     }
 
-    fun registerGame(game: Game): Game {
-        val key = "${game.teamAmount}x${game.teamSize}"
-        games.getOrPut(key) { mutableListOf() }.apply {
-            add(game)
-        }
-        gameIDs.add(game.id)
-        return game
-    }
-
-    fun unregisterGame(game: Game): Boolean {
-        val key = "${game.teamAmount}x${game.teamSize}"
-        val removed = games[key]?.remove(game) ?: false
-        if (removed && games[key].isNullOrEmpty()) {
-            games.remove(key)
-        }
-        gameIDs.remove(game.id)
-        return removed
-    }
+    fun registerGame(game: Game): Game = game.also { games.add(game) }
+    fun unregisterGame(game: Game): Boolean = games.remove(game)
 }
