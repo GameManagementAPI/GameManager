817abf4e939e8d8c1da5647097d1b4eb62baa12e
c4vxl <manage@c4vxl.de>
2024-08-31 12:44:38 +0200

    update GameManagementAPI

--- src/main/kotlin/de/c4vxl/gamemanager/gamemanagementapi/GameManagementAPI.kt
+++ src/main/kotlin/de/c4vxl/gamemanager/gamemanagementapi/GameManagementAPI.kt
@@ -2,10 +2,14 @@ package de.c4vxl.gamemanager.gamemanagementapi
 
 import de.c4vxl.gamemanager.gamemanagementapi.game.Game
 import de.c4vxl.gamemanager.gamemanagementapi.game.GameID
+import de.c4vxl.gamemanager.gamemanagementapi.world.WorldManager
+import java.io.File
 
 object GameManagementAPI {
     val games: MutableList<Game> = mutableListOf()
     private val gameIDs: MutableList<GameID> get() = games.map { it.id }.toMutableList()
+
+    val possibleGames: MutableList<String> = File(WorldManager.mapsContainerPath).list()?.toMutableList() ?: mutableListOf()
 
     fun getGames(teamAmount: Int, teamSize: Int): List<Game> = games.filter { it.teamAmount == teamAmount && it.teamSize == teamSize }
 
@@ -13,8 +17,10 @@ object GameManagementAPI {
 
     fun getGame(teamAmount: Int, teamSize: Int): Game {
         return getGames(teamAmount, teamSize).filter { it.isQueuing }.randomOrNull()
-            ?: registerGame(Game(teamAmount, teamSize))
+            ?: createGame(teamAmount, teamSize)
     }
+
+    fun createGame(teamAmount: Int, teamSize: Int): Game = registerGame(Game(teamAmount, teamSize))
 
     fun registerGame(game: Game): Game = game.also { games.add(game) }
     fun unregisterGame(game: Game): Boolean = games.remove(game)
