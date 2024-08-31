d3a336593575150e350cf3e4b76d97a7ecf05ac7
c4vxl <manage@c4vxl.de>
2024-08-31 20:37:22 +0200

    introduced Win/Loose events

--- src/main/kotlin/de/c4vxl/gamemanager/gamemanagementapi/event/Events.kt
+++ src/main/kotlin/de/c4vxl/gamemanager/gamemanagementapi/event/Events.kt
@@ -8,6 +8,7 @@ import de.c4vxl.gamemanager.gamemanagementapi.world.MapConfig
 import org.bukkit.World
 
 // game-handling based events
+data class GamePreStopEvent(val game: Game): GameEvent()
 data class GameStopEvent(val game: Game, var kickPlayers: Boolean = true): GameEvent()
 data class GameStartEvent(val game: Game): GameEvent()
 data class GameForceStopEvent(val game: Game): GameEvent()
@@ -18,10 +19,14 @@ data class GamePlayerQuitEvent(val player: GMAPlayer, val game: Game): GameEvent
 data class GamePlayerJoinEvent(val player: GMAPlayer, val game: Game): GameEvent()
 data class GamePlayerEliminateEvent(val player: GMAPlayer, val game: Game): GameEvent()
 data class GamePlayerReviveEvent(val player: GMAPlayer, val game: Game): GameEvent()
+data class GamePlayerWinEvent(val player: GMAPlayer, val game: Game): GameEvent()
+data class GamePlayerLooseEvent(val player: GMAPlayer, val game: Game): GameEvent()
 
 // team based events
 data class GamePlayerTeamJoinEvent(val player: GMAPlayer, val game: Game, val team: Team): GameEvent()
 data class GamePlayerTeamQuitEvent(val player: GMAPlayer, val game: Game, val team: Team): GameEvent()
+data class GameTeamWinEvent(val player: GMAPlayer, val game: Game): GameEvent()
+data class GameTeamLooseEvent(val player: GMAPlayer, val game: Game): GameEvent()
 
 // world based events
 data class GameWorldMapForceEvent(val game: Game, var forceTo: String?): GameEvent()
