f51865afb30f7c23ac7885e531211f27c27bf8b7
c4vxl <manage@c4vxl.de>
2024-11-02 21:46:07 +0100

    bug fix in queue countdown

--- src/main/kotlin/de/c4vxl/gamemanager/plugin/handlers/QueueHandler.kt
+++ src/main/kotlin/de/c4vxl/gamemanager/plugin/handlers/QueueHandler.kt
@@ -10,7 +10,6 @@ import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.NamedTextColor
 import org.bukkit.Bukkit
 import org.bukkit.boss.BarColor
-import org.bukkit.boss.BarFlag
 import org.bukkit.boss.BarStyle
 import org.bukkit.boss.BossBar
 import org.bukkit.event.EventHandler
@@ -38,36 +37,36 @@ class QueueHandler(plugin: Plugin) : Listener {
         Bukkit.getPluginManager().registerEvents(this, plugin)
     }
 
-    private val countdowns: MutableMap<Game, Int> = mutableMapOf()
+    private val countdowns: MutableMap<Game, Pair<Int, Int>> = mutableMapOf()
     private val bars: MutableMap<Game, BossBar> = mutableMapOf()
 
     private fun startCountdown(game: Game, seconds: Int) {
-        countdowns[game] = seconds
+        countdowns[game] = Pair(seconds, seconds)
 
         val bar: BossBar = Bukkit.createBossBar("Game starts in: $seconds", BarColor.GREEN, BarStyle.SOLID)
         bars[game] = bar
         game.players.forEach { bar.addPlayer(it.bukkitPlayer) }
 
         Bukkit.getScheduler().runTaskTimer(GameManager.instance, { task ->
-            val ticks = countdowns[game] ?: 0
+            val c = countdowns[game] ?: Pair(0, 1)
 
-            if (ticks <= 0) {
+            if (c.first <= 0) {
                 bar.removeAll()
                 game.start()
                 task.cancel()
                 return@runTaskTimer
             }
 
-            bar.setTitle("Game starts in: $ticks")
-            bar.progress = ticks.toDouble() / seconds
+            bar.setTitle("Game starts in: ${c.first}")
+            bar.progress = c.first.toDouble() / c.second
 
-            countdowns[game] = countdowns[game]!! - 1
+            countdowns[game] = Pair(c.first - 1, c.second)
         }, 0, 20)
     }
 
     private fun setCountdown(game: Game, seconds: Int) {
         if (countdowns.containsKey(game))
-            countdowns[game] = seconds
+            countdowns[game] = Pair(seconds, seconds)
         else
             startCountdown(game, seconds)
     }
@@ -76,9 +75,9 @@ class QueueHandler(plugin: Plugin) : Listener {
         val maxPlayers: Int = game.maxPlayers
 
         return when (game.players.size) {
-            (maxPlayers * 0.25).toInt() -> 0.25
+            (maxPlayers * 0.25).toInt() -> 0.75
             (maxPlayers * 0.5).toInt() -> 0.5
-            (maxPlayers * 0.75).toInt() -> 0.7
+            (maxPlayers * 0.75).toInt() -> 0.25
             else -> 1.0
         }
     }
