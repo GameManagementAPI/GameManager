f9bb6579ae7405840e86ca4d8472ad405f1c49a9
c4vxl <manage@c4vxl.de>
2026-02-16 22:55:56 +0100

    Added GMAPlayer

---
+++ src/main/kotlin/de/c4vxl/gamemanager/gma/player/GMAPlayer.kt
@@ -0,0 +1,11 @@
+package de.c4vxl.gamemanager.gma.player
+
+import de.c4vxl.gamemanager.gma.game.Game
+import org.bukkit.entity.Player
+
+class GMAPlayer(
+    val bukkitPlayer: Player,
+    var game: Game? = null
+) {
+
+}
