0359f7b0cade065f4f9b7a03df569e47103794dc
c4vxl <manage@c4vxl.de>
2026-02-17 00:37:11 +0100

    Added Resource Utilities

---
+++ src/main/kotlin/de/c4vxl/gamemanager/utils/ResourceUtils.kt
@@ -0,0 +1,31 @@
+package de.c4vxl.gamemanager.utils
+
+import de.c4vxl.gamemanager.Main
+import java.nio.file.Files
+import java.nio.file.Path
+import kotlin.io.path.name
+
+object ResourceUtils {
+    /**
+     * Reads the content of a jar-packed resource
+     * @param path The path to the resource
+     */
+    fun readResource(path: String): String =
+        ResourceUtils.javaClass.getResourceAsStream("/$path")?.bufferedReader()?.readText() ?: ""
+
+    /**
+     * Saves a jar-packed resource to disk
+     * @param path The path on disk
+     */
+    fun saveResource(path: String, destination: String? = null, replace: Boolean = false) {
+        val destPath = Path.of(destination ?: Main.instance.dataPath.resolve(Path.of(path).name).toString())
+
+        destPath.parent?.toFile()?.mkdirs()
+
+        if (replace) Files.deleteIfExists(destPath)
+
+        ResourceUtils.javaClass.getResourceAsStream("/$path")
+            ?.use { Files.copy(it, destPath) }
+            ?: error("Resource /$path not found!")
+    }
+}
