da0d7fac4a5af5d543709c5e4aa727f44692cb38
c4vxl <manage@c4vxl.de>
2026-02-17 02:03:37 +0100

    Updated GameState labels

--- src/main/kotlin/de/c4vxl/gamemanager/gma/game/type/GameState.kt
+++ src/main/kotlin/de/c4vxl/gamemanager/gma/game/type/GameState.kt
@@ -4,9 +4,9 @@ package de.c4vxl.gamemanager.gma.game.type
  * A list of different states a game could be in
  */
 enum class GameState(val label: String) {
-    QUEUING("<grey>Queuing</grey>"),
-    STARTING("<dark_green>Starting</dark_green>"),
-    RUNNING("<green>Running</green>"),
-    STOPPING("<dark_red>Stopping</dark_red>"),
-    STOPPED("<red>Stopped</red>")
+    QUEUING("<gold>■ Queuing</gold>"),
+    STARTING("<dark_green>■ Starting</dark_green>"),
+    RUNNING("<green>■ Running</green>"),
+    STOPPING("<dark_red>■ Stopping</dark_red>"),
+    STOPPED("<red>■ Stopped</red>")
 }
