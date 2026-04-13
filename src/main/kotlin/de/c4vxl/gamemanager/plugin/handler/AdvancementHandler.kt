c083de7b3d9c076fb280a7a1c1581805a93f4992
c4vxl <manage@c4vxl.de>
2026-04-13 14:26:27 +0200

    Fixed AdvancementHandler calling itself

--- src/main/kotlin/de/c4vxl/gamemanager/plugin/handler/AdvancementHandler.kt
+++ src/main/kotlin/de/c4vxl/gamemanager/plugin/handler/AdvancementHandler.kt
@@ -2,6 +2,8 @@ package de.c4vxl.gamemanager.plugin.handler
 
 import de.c4vxl.gamemanager.GameManager
 import de.c4vxl.gamemanager.gma.player.GMAPlayer.Companion.gma
+import io.papermc.paper.configuration.constraint.Constraints.Min
+import net.kyori.adventure.text.minimessage.MiniMessage
 import org.bukkit.Bukkit
 import org.bukkit.GameRules
 import org.bukkit.event.EventHandler
@@ -25,18 +27,27 @@ class AdvancementHandler : Listener {
         if (world.getGameRuleValue(GameRules.SHOW_ADVANCEMENT_MESSAGES) != false)
             return
 
-        // Hacky way to reset the xp a challenge advancement might have given
-        val currentLevel = event.player.level
-        val currentXP = event.player.exp
-        Bukkit.getScheduler().callSyncMethod(GameManager.instance) {
-            event.player.level = currentLevel
-            event.player.exp = currentXP
-        }
+        val progress = event.player.getAdvancementProgress(event.advancement)
+
+        // Return if advancement isn't done
+        if (!progress.isDone)
+            return
+
+        // Advancement doesn't show
+        if (event.advancement.display?.doesShowToast() != true)
+            return
 
         // Hacky way to disable the advancement popup in the top right corner
-        val progress = event.player.getAdvancementProgress(event.advancement)
         for (criterion in progress.awardedCriteria) {
             progress.revokeCriteria(criterion)
         }
+
+        // Hacky way to reset the xp a challenge advancement might have given
+        val currentLevel = event.player.level
+        val currentXP = event.player.exp
+        Bukkit.getScheduler().runTask(GameManager.instance, Runnable {
+            event.player.level = currentLevel
+            event.player.exp = currentXP
+        })
     }
 }
